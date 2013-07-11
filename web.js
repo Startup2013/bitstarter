05c13851c610c453861a88cbe0287bf96269b3ec
Startup 2013 <startup20130617@gmail.com>
2013-07-12 02:04:11 +0300

    Second Submission

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
-app.configure(function(){
-app.use(express.static(__dirname + '/public/docs/'));
-});
+//for later use, seems tried to hard:D
+//app.configure(function(){
+//app.use(express.static(__dirname + '/public/docs/'));
+//});
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('public/docs/index.html','utf-8')) ;
+  response.send(fs.readFileSync('index.html','utf-8')) ;
 });
 
 var port = process.env.PORT || 5000;
