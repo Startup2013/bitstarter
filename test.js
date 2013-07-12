cf7e1436890cd31dc94d003308c078086a1edf09
Startup 2013 <startup20130617@gmail.com>
2013-07-12 19:43:12 +0300

    Working HW3, not polished, but submited

---
+++ test.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-s, --sessions', 'add session support')
+  .option('-t, --template <engine>', 'specify template engine (jade|ejs) [jade]', 'jade')
+  .option('-c, --css <engine>', 'specify stylesheet engine (stylus|sass|less) [css]', 'css')
+  .parse(process.argv);
+
+console.log(' - sessions %j', program.sessions);
+console.log(' - template %j', program.template);
+console.log(' - css %j', program.css);
