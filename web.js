76060931797e1b20b916f62db42af6f17b25eb81
Startup <startup20130617@gmail.com>
2013-08-02 14:58:00 +0000

    New landing page

--- web.js
+++ web.js
@@ -2,14 +2,25 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-//for later use, seems tried to hard:D
-//app.configure(function(){
-//app.use(express.static(__dirname + '/public/docs/'));
-//});
+ app.configure(function(){
+ app.use(express.static(__dirname + '/dist/'));
+ app.use(express.bodyParser());
+});
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html','utf-8')) ;
 });
+
+app.post('/subscribe', function(request, response) {
+   var suldubuldu = request.body;
+   console.log(suldubuldu['email_address']);
+   fs.appendFileSync("emails.txt", suldubuldu['email_address']+"\n");   
+//response.send({
+     //retStatus: "Success"
+   //});
+   response.redirect('/'); 
+});
+    
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
